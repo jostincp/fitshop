15602f59c1887a6836b96d5990a2044335fd3a24
jostincp <jostincastillop@hotmail.com>
2023-05-28 20:33:51 -0500

    Creación del modelo de usuario predeterminado de Django

--- ecommercefitshop/ecommercefitshop/settings.py
+++ ecommercefitshop/ecommercefitshop/settings.py
@@ -118,6 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Especificar el modelo de usuario predeterminado de Django
+AUTH_USER_MODEL = 'auth.User'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- ecommercefitshop/orders/models.py
+++ ecommercefitshop/orders/models.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Crea tus modelos aquí.
 class Ejemplo(models.Model):
-    name = models.CharField(max_length=50,default='')
-    description = models.CharField(max_length=200,default='')
+    name = models.CharField(max_length=50, default='')
+    description = models.CharField(max_length=200, default='')
     number = models.IntegerField(default=0)
+#
