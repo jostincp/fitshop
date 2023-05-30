37af8154f649fc1181743221c63f85a66c43c0c2
jostincp <jostincastillop@hotmail.com>
2023-05-29 21:59:10 -0500

    Se crea tabla de productos

--- ecommercefitshop/orders/models.py
+++ ecommercefitshop/orders/models.py
@@ -5,4 +5,13 @@ class Ejemplo(models.Model):
     name = models.CharField(max_length=50, default='')
     description = models.CharField(max_length=200, default='')
     number = models.IntegerField(default=0)
-#
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity_available = models.IntegerField()
+    image = models.ImageField(upload_to='product_images/')
+
+    def __str__(self):
+        return self.name
